e332e10735e5e48649ac88645047f65fdab5a272
konda Srikanth <kondasrikanth2006@gmail.com>
2026-07-09 11:32:38 +0530

    Add files via upload

---
+++ swaping.py
@@ -0,0 +1,223 @@
+#swaping of two integers
+#using assignment
+'''a=10
+b=20
+a,b=b,a
+print("a value is",a)
+print("b value is ",b)
+
+
+#by using the tempt variable
+a=10
+b=20
+tempt=a
+a=b
+b=tempt
+print("a value is",a)
+print("b value is",b)
+
+#arthematic methods
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("a value is",a)
+print("b value is",b)
+
+#formatting methpds
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swaping a=%d,b=%d"%(a,b))
+
+
+
+#swaping of two float values
+a=2.3
+b=4.5
+a,b=b,a
+print("a value is",a)
+print("b value is",b)
+
+#arthematic method
+a=9.5
+b=8.5
+a=a+b
+b=a-b
+a=a-b
+print("a value is",a)
+print("b value is",b)
+
+#tempt variable
+a=4.5
+b=5.5
+tempt=a
+a=b
+b=tempt
+print("a value is",a)
+print("b value is",b)
+
+#formatting method
+a=2.4
+b=9.8
+a=a+b
+b=a-b
+a=a-b
+print("after swaping a=%.2f,b=%.2f"%(a,b))'''
+
+#swaping of two strings
+#tempt variable
+'''a="sri"
+b="ram"
+tempt=a
+a=b
+b=tempt
+print(a)
+print(b)
+
+
+a="sai"
+b="konda"
+a,b=b,a
+print(a)
+print(b)
+
+
+#formatting
+a="sri"
+b="sai"
+print("after swaping a=%s,b=%s"%(b,a))
+
+#or
+a="sri"
+b="sai"
+print("after swaping a={} b={}".format(b,a))
+
+#f-string
+a="sri"
+b="sai"
+print(f"a={b},b={a}")'''
+
+
+#swaping of two complex
+a=2+4j
+b=5+5j
+a,b=b,a
+print(a)
+print(b)
+
+
+a=2+4j
+b=5+5j
+tempt=a
+a=b
+b=tempt
+print(a)
+print(b)
+
+a=2+4j
+b=5+5j
+a=a+b
+b=a-b
+a=a-b
+print(a)
+print(b)
+
+a=2+4j
+b=5+5j
+print(f"a={b} , b={a}")
+
+a=2+4j
+b=5+5j
+print("a={},b={}".format(b,a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
