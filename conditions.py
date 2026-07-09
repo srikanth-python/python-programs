d34e269058d1f0fa20e5475940f5a75dbad0fdec
konda Srikanth <kondasrikanth2006@gmail.com>
2026-07-09 11:33:09 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,275 @@
+#conditions
+#if-condition by using comparision operators
+'''a=2
+b=4
+if a<b:
+    print("true")
+a=2
+b=4
+if a>b:
+    print("true")
+a=12
+b=14
+if a<=b:
+    print("true")
+
+a=20
+b=40
+if b>=a:
+    print("true")
+
+    
+a=7
+b=9
+if a!=b:
+    print("not equal")
+
+    
+a=5
+b=10
+if a==b:
+    print("equal")
+
+
+a=9
+b=9
+if a==b:
+    print("true")
+
+
+a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")
+
+    
+a=int(input("a value"))
+if a<10:
+    print("less")
+    
+
+a="python"
+if a=="java":
+    print("true")
+
+a=input("data")
+if a=="java":
+    print("true")
+
+#if-condition by using logical operators
+#and,or,not
+
+a=4
+b=5
+if a<b and b>a:
+    print("true")
+
+
+a=4
+b=9
+if a<=b and b>=a:
+    print("true")
+
+a=4
+b=9
+if a!=b and a==b:
+    print("true")
+
+a=4
+b=9
+if a<b or b>a:
+    print("true")
+
+a=4
+b=9
+if a<=b or b>=a:
+    print("true")
+
+
+a=3
+b=3
+if a==b or a!=b:
+    print("true")
+
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+if a==b or a!=b:
+    print("true")
+
+a=13
+b=15
+if not a<b:
+    print("less")
+
+a=13
+b=15
+if not a>b:
+    print("less")
+
+a=13
+b=15
+if not a<b and b>a:
+    print("less")
+
+a=13
+b=15
+if not a<b or b>a:
+    print("less")
+
+
+a=int(input())
+b=int(input())
+if a<b and b>a:
+    print("less")
+
+#if-condition by using identify operators
+#is, is not
+a=5
+if type(a) is init:
+    print("it is int")
+
+a=6
+if type(a) is not int:
+    print("false")
+
+a=int(input("a value"))
+if type(a) is int:
+    print("it is int")
+
+#same for float string
+#if-condition by using membership operators
+#in,not in
+a=2,3,4,5,6,7,8,9
+if 20 in a:
+    print("true")
+
+a=2,3,4,5,6,7,8,9
+if 20 in a:
+    print("true")
+
+
+a=2,3,4,5,6,7,8,9
+if 20 not in a:
+    print("true")
+
+a=int(input("enter the value"))
+if 30 in a:
+    print("true") #error'''
+
+
+a=2,3,4,5,6,7,8,9
+b=int(input("a value"))
+if b in a:
+    print("true")
+
+
+
+    
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
