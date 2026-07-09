177e956ea838196d576dc913430015a52cce845e
konda Srikanth <kondasrikanth2006@gmail.com>
2026-07-09 11:30:45 +0530

    Add files via upload

---
+++ runtimeinputs.py
@@ -0,0 +1,148 @@
+'''a=2
+b=4
+print(a+b)'''
+
+#run_time input()
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("data 1"))
+b=str(input("data 2"))
+print(a+" "+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''a=complex(input("enter the a value"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+
+'''a=input("enter a value")
+b=input("enter b value")
+print(a+b)
+
+fname=input("first name")
+lname=input("last name")
+print((fname+" "+lname).title())'''
+
+'''a=int(input("enter a value"))
+b=int(input("enter b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=input("choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=input(choose the option
+         1.add
+         2.sub
+         3.mul)
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+
+
+'''#swaping of two numbers integers
+#method1
+a=10
+b=20
+a,b=b,a
+print("a value is",a)
+print("b value is",b)
+
+#method2
+a=10
+b=20
+tempt=a
+a=b
+b=tempt
+print("a value is",a)
+print("b value is",b)
+
+#method3
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("a value is",a)
+print("b value is",b)
+
+#method4
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d"%(a,b))'''
+
+#swaping two numbers float
+a=float(input("a value"))
+b=float(input("b value"))
+a=a+b
+b=a-b
+a=a-b
+print("after swaping a=%.2f,b=%.2f"%(a,b))
+
+
+
+a=input("data1")
+b=input("data2")
+tempt=a
+a=b
+b=tempt
+print(%(a,b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
