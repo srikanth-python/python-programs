f1e498e5e18067865027fcb1585090b789117fd2
konda Srikanth <kondasrikanth2006@gmail.com>
2026-08-09 22:48:43 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,31 @@
+'''import modules
+modules.greetings("srikanth")'''
+
+#import modules
+
+
+#import modules
+
+'''import modules
+a=modules.details["name"]
+print(a)'''
+#in dictionary only one key is access not multiple keys
+'''b=modules.details["name","rollnos"] #error
+print(b)'''
+
+
+'''import modules'''
+
+#import modules
+'''import modules
+modules.add()'''
+
+#import modules
+
+'''import modules
+modules.add()'''
+
+import modules
+a=modules.a["name"]
+print(a)
+#it is impossible to access multi keys at a time
