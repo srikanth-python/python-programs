4e55b1511aaf21ff6bba3f5b225c0a58169af969
konda Srikanth <kondasrikanth2006@gmail.com>
2026-08-09 22:46:23 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,189 @@
+#loops
+#for,while,range,break,continue,pass
+#for loop()->it will print the sequence of actions
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+
+
+a=[10,20,30,40,50,60]
+for i in a:
+    print(i,end=" ")
+
+a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+    
+    
+a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))
+
+
+a=(4,5,6,7,8,9)
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))
+    
+
+a={4,5,6,7,8,9}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))
+
+d={"year":2026,"month":"july","date":10}
+for i in d:
+    print(i)
+for i in d.keys():
+    print(i)
+    print(type(d))
+    print(type(i))
+for i in d.values():
+     print(i)
+    print(type(d))
+    print(type(i))
+for i in d.items():
+     print(i)
+    print(type(d))
+    print(type(i))
+
+
+
+a="codegnan"
+for i in a:
+    print(i)
+
+a=[3.4,5.6]
+for i in a:
+    print(i)
+print(type(i))
+print(type(a))
+
+
+
+a=["pyhton","java","html","css"]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))
+
+
+a=[4+5j,2+5j]
+for i in a:
+     print(i)
+print(type(a))
+print(type(i))
+
+
+a=[True,False]
+for i in a:
+     print(i)
+print(type(a))
+print(type(i))
+
+
+#task1
+fruits=["apple","bananna","mango"]
+b=str(fruits)
+c=b.upper()
+print(c)
+#["APPLE","BANANNA","MANGO"]
+
+
+#by using for loop
+a=["apple","bananna","mango"]
+for i in a:
+    print(i.upper(),end=",")#here the list is not coming
+
+
+a=["apple","bananna","mango"]
+b=[]
+for i in a:
+    b.append(i.upper())
+print(b)   
+    
+#task2
+a=[10,20,30,40,50,"code"]
+#["10,20,30,40,50,"code","c","o","d","e"]
+a.extend("code")
+print(a)
+
+a=[2,3,5,6,7]
+#[2,3,4,5,6,7]
+a.insert(2,4)
+print(a)
+
+b=(5,6,7,8,9,10)
+#(5,6,7,8,9)
+c=list(b)
+c.remove(10)
+d=tuple(c)
+print(d)'''
+
+
+'''e=[7,9,2,0,1,4,9,3,20]
+e.sort()
+print(e)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
