6b271e8012e9ab77336108514de416a364f0da96
konda Srikanth <kondasrikanth2006@gmail.com>
2026-08-09 22:37:33 +0530

    Add files via upload

---
+++ conditions1.py
@@ -0,0 +1,353 @@
+#if-else conditions by using comparison operators
+'''a=3
+b=6
+if a<b:
+    print("true")
+else:
+    print("False")
+
+
+a=3
+b=6
+if a>b:
+    print("true")
+else:
+    print("false")
+
+
+a=9
+b=12
+if a!=b:
+    print("not equal")
+else:
+    print("true")
+
+#if-else conditions by using logical operators
+a=6
+b=10
+if a<b and b>a:
+    print("less")
+else:
+    print("true")
+
+#if-else condition by using identify operators
+a=10
+b=20
+if type(a) is int:
+    print("true")
+else:
+    print("false")
+
+
+a=20
+b=30
+if type(a) is not int:
+    print("true")
+else:
+    print("false")
+
+#if-else condition by using membership operators
+a=1,2,3,4,5,6,7
+if 2 in a:
+    print("true")
+else:
+    print("false")
+
+
+a=[1,2,34,5,6,7,8]
+if 10 not in a:
+    print("true")
+else:
+    print("false")
+
+a=2,3,4,5,6
+if 2 not in a:
+    print("true")
+else:
+    print("false")
+
+#if-elif-else conditions by using comparision operators
+a=8
+b=20
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    print("true")
+
+
+a=8
+b=10
+if a==b:
+    print("less")
+elif b>a:
+    print("greater")
+else:
+    
+    print("true")
+
+a=8
+b=10
+if a==b:
+    print("less")
+elif b<a:
+    print("greater")
+else:
+    print("true")
+
+
+a=10
+b=20
+if a>b:
+    print("less")
+elif b<a:
+    print("greate")
+elif a!=b:
+    print("not equal")
+else:
+    print("true")
+
+#if-elif-else conditions by using logical operators
+a=10
+b=20
+if a>b and b<a:
+    print("less")
+elif a==b and a!=b:
+    print("not equal")
+elif a<b and b==a:
+    print("greater")
+elif a<b and a!=b:
+    print("it is true")
+else:
+    print("false")
+
+
+
+a=10
+b=20
+if a>b or b<a:
+    print("less")
+elif a==b or a>b:
+    print("greater")
+else:
+    print("true")
+
+
+
+#if-elif-else conditions by using membership operators
+a=10,20,30,40,50,60
+b=int(input("enter the number:"))
+if b in a:
+    print("true")
+else:
+    print("false")
+      
+
+a=10,20,30,40,50,60
+b=int(input("enter the number:"))
+if b not in a:
+    print("false")
+else:
+    print("true")
+
+
+
+#mutiple if conditions
+a=20
+b=30
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")
+else:
+    print("true")
+
+a=30
+b=60
+if a<b:
+    print("less")
+if a!=b:
+    print("greater")
+if a==b:
+    print("not equal")
+else:
+    print("true")
+        
+    
+    
+    
+if a>=b:
+    print("less")
+if b<a:
+    print("greater")
+if a==b:
+    print("not equal")
+else:
+    print("true")
+
+#nested-if->means if inside another if conditions taken it is called nested if
+a=4
+b=6
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")
+
+a=4
+b=6
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")
+
+a=7
+b=12
+if a<b:
+    print("less")
+    if b==a:
+        print("equal")
+
+a=12
+b=13
+if a<b:
+    print("less")
+    if b==a:
+        print("equal")
+    else:
+        print("true")
+a=15
+b=17
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")
+else:
+    print("true")
+
+a=9
+b=11
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")
+    else:
+        print("true")
+else:
+    print("false")
+
+a=4
+b=6
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")
+    elif a!=b:
+        print("not equal")
+
+a=4
+b=6
+if a<b:
+    print("less")
+    if b==a:
+        print("greater")
+    elif a>=b:
+        print("not equal")
+    else:
+        print("equal")'''
+
+'''a=4
+b=6
+if a>b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+
+'''a=4
+b=6
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+    
+    
+
+
+
+
+
+    
+    
